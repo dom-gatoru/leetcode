6321fc42937a1ab17b2bc061cad93305c1d1088c
dom-gatoru <amano@DESKTOP-0A2NSD4.localdomain>
2021-02-16 23:46:05 +0900

    Two Sum(php)

---
+++ two-sum/Solution.php
@@ -0,0 +1,28 @@
+<?php
+class Solution {
+
+    function main(){
+
+    }
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+     function towSum($nums, $target) {
+
+        $array = array();
+        
+        foreach ($nums as $index => $num) {
+            $key = $target - $num;
+
+            if(!array_key_exists($key, $nums)){
+                $array[$num] = $index;
+            } else {
+                return [$index, $array[$key]];
+            }
+        }
+
+     }
+}
