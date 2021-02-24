d904415337a024390b9c745ae71786aac4611d14
dom-gatoru <pandan0308@gmail.com>
2021-02-25 00:55:15 +0900

    if文追加

--- palindrome-number/Solution.java
+++ palindrome-number/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
+        if(x < 0) return false;
+
         StringBuffer sb = new StringBuffer(String.valueOf(x));
         if(String.valueOf(x).equals(sb.reverse().toString())){
             return true;
@@ -8,5 +10,5 @@ class Solution {
             return false;
         }
     }
-    
+
 }
